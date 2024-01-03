c2f87f670f7cbad604d71195c48c51a8b21b2647
Angry_Bird <duranjai000@gmail.com>
2024-01-03 19:03:58 +0530

    Added Guess the Number (Computer)

---
+++ 2_guess_the_numer_computer.py
@@ -0,0 +1,16 @@
+from random import randint
+iMin = int(input("Enter the minimum number: "))
+iMax = int(input("Enter the maximum number: "))
+while (iMax < iMin):
+    print("Invalid entry!!! Enter again")
+    iMin = int(input("Enter the minimum number: "))
+    iMax = int(input("Enter the maximum number: "))
+randNum = randint(iMin, iMax)
+iGuess = None
+while(iGuess != randNum):
+    iGuess = int(input("Enter your guess: "))
+    if (iGuess > randNum):
+        print("Guess lower")
+    elif (iGuess < randNum):
+        print("Guess higher")
+print("Your guess is correct!!!")
