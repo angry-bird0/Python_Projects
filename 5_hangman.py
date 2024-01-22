c46b63389dfd24dc8c5a7eb5ea055b7123fbb453
angry_bird <76245421+angry-bird0@users.noreply.github.com>
2024-01-22 22:23:48 +0530

    Updated documentation for 5

--- 5_hangman.py
+++ 5_hangman.py
@@ -1,7 +1,7 @@
 # DOCUMENTATION
 # This program asks the user for the number of lives and selects a random word from a list of 100 words, and the user has to play the game 'hangman' where they guess which word it is.
 
-# Functions
+# FUNCTIONS:
 
 # CODE
 from random import choice
