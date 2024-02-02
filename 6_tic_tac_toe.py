53f6e894ae33d9884914187c410a55b674e64c82
Angry_Bird <duranjai000@gmail.com>
2024-02-02 23:39:36 +0530

    Updated 6

--- 6_tic_tac_toe.py
+++ 6_tic_tac_toe.py
@@ -1,6 +1,8 @@
 # DOCUMENTATION
 # This program simulates a game of tic-tac-toe between different players which can be humans, computers (randomly selection blocks), or computers (using mini-max algorithm so that the computer never loses)
 
+# CLASSES:
+
 # FUNCTIONS:
 
 # VARIABLES:
