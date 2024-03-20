fcf7c261b1a4a12ccced1c920da06efbec13e5eb
Angry_Bird <duranjai000@gmail.com>
2024-03-20 21:27:46 +0530

    Updated 5

--- 5_hangman.py
+++ 5_hangman.py
@@ -9,7 +9,7 @@
 # iWord (string): A word randomly chosen from wordList.
 # wordLetters (set): Set of unique letters in iWord.
 # remainingAlphabets (list): List of alphabets not tried by the user.
-# iLives (integer): Stores the number of lives the user has
+# iLives (integer): Stores the number of lives the user has.
 # userLetter (string): Input letter from the user
 
 # CODE
