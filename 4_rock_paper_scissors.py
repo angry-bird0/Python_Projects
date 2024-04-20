f0be8749709c570834f5b0b2aa5290e80e2e7206
angry_bird <76245421+angry-bird0@users.noreply.github.com>
2024-04-20 23:40:39 +0530

    Update 4_rock_paper_scissors.py

--- 4_rock_paper_scissors.py
+++ 4_rock_paper_scissors.py
@@ -2,7 +2,7 @@
 # This program takes input from the user and compares it with a random choice (between rock, paper, and scissor) made by the compter
 
 # FUNCTIONS:
-# choice (List[String]) -> (String): Choses and returns a pseudo-random element from that list.
+# choice (List[String]) -> (String): Choses and returns a pseudo-random element from that list
 
 # VARIABLES:
 # userInput (string): Stores input from the user.
